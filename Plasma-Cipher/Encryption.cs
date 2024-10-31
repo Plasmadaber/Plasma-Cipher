cad1769ff7cbece510efaf2ddab4d573b57602ba
Channing Monsch <plasmastudioscontact@gmail.com>
2024-10-31 10:07:16 -0400

    Intial Commit - Commiting all files

---
+++ Plasma-Cipher/Encryption.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace PlasmaCipher
+{
+	public class Encryption
+	{
+		public Encryption(int key)
+		{
+
+		}
+	}
+
+	public class Decryption
+	{
+		public Decryption(int key)
+		{
+
+		}
+	}
+
+	public static class Keys
+	{
+
+	}
+}
+
---
+++ Plasma-Cipher/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace Plasma_Cipher;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
