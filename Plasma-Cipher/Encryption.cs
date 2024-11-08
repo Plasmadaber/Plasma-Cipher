d0f4b326cbb9d1555dcd84694f8c1f6994a42ae6
Channing Monsch <plasmastudioscontact@gmail.com>
2024-11-08 13:58:44 -0500

    Encryption - Starting code for Encryption and Decryption completed

--- Plasma-Cipher/Encryption.cs
+++ Plasma-Cipher/Encryption.cs
@@ -1,25 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Runtime.InteropServices;
+
 namespace PlasmaCipher
 {
 	public class Encryption
 	{
+		private string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+-=[]\\{}:;\"/.,<>|`'~? èéêëěẽēėęàáâäǎæãåāòóôöǒœøõōìíîïǐĩīıįùúûüǔũūűůÈÉÊËĚẼĒĖĘÀÁÂÄǍÆÃÅĀÒÓÔÖǑŒØÕŌÌÍÎÏǏĨĪİĮÙÚÛÜǓŨŪŰŮýŷÿÝŶŸ";
+		private int _key;
+
 		public Encryption(int key)
 		{
-
+			this._key = key;
 		}
+
+		public string EncryptMessage(string message)
+		{
+			List<char> letters = message.ToList();
+			string newMess = "";
+			foreach (char letter in letters)
+			{
+				newMess += EncryptChar(letter);
+			}
+			return newMess;
+		}
+
+		private char EncryptChar(char letter)
+		{
+            int newIndex = chars.IndexOf(letter) ^ _key;
+			Console.WriteLine(letter);
+            return chars[newIndex];
+        }
+
 	}
 
 	public class Decryption
 	{
-		public Decryption(int key)
+        private string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+-=[]\\{}:;\"/.,<>|`'~? èéêëěẽēėęàáâäǎæãåāòóôöǒœøõōìíîïǐĩīıįùúûüǔũūűůÈÉÊËĚẼĒĖĘÀÁÂÄǍÆÃÅĀÒÓÔÖǑŒØÕŌÌÍÎÏǏĨĪİĮÙÚÛÜǓŨŪŰŮýŷÿÝŶŸ";
+		private int _key;
+        public Decryption(int key)
 		{
-
+			this._key = key;
 		}
-	}
 
-	public static class Keys
-	{
+		public string DecryptMessage(string message)
+		{
+            List<char> letters = message.ToList();
+            string oldMess = "";
+            foreach (char letter in letters)
+            {
+                oldMess += DecryptChar(letter);
+            }
+            return oldMess;
+        }
 
+		private char DecryptChar(char letter)
+		{
+            int oldIndex = chars.IndexOf(letter) ^ _key;
+            return chars[oldIndex];
+        }
 	}
 }
 
